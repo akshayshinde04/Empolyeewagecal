7644d4af603a752d348c2cbb046e6131ff62a01d
unknown <akshayHVshinde04@gmail.com>
2021-09-25 16:43:50 +0530

    Added: EmpolyeewagecalUC1

--- Employeewagecal.java
+++ Employeewagecal.java
@@ -3,5 +3,20 @@ package pratice;
 public class Employeewagecal {
 	public static void main(String[] args) { 
 		System.out.println(" Welcome to Employee Wages");
+		
+		int num = (int)(Math.random()*10 % 2);
+
+		if(num == 1)
+		
+			System.out.println("employee is present");
+		
+		else 
+	
+			System.out.println("employee is not present");
+		
+		
+		
+		
+		
  }
 }
