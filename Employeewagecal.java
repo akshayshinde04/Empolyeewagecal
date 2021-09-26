f6bf677ebdfa7a14859a80621c3f20d4f2a22f1b
unknown <akshayHVshinde04@gmail.com>
2021-09-26 20:55:12 +0530

    Added: EmployeeWage20UC5

--- Employeewagecal.java
+++ Employeewagecal.java
@@ -2,23 +2,30 @@ package pratice;
 
 public class Employeewagecal {
 	public static void main(String[] args) { 
-		int part_time_hour,wage_per_hour =20,Cal,full_day;
+	
+		int empWage;
+		int empRatePerHr = 20;
+		int emphour = 0;
+		int wage_per_hour = 20;
+		int empDay = 20;
+	
+		 int empCheck = (int)(Math.random()*10) % 3;
+	       switch(empCheck)
+	       {
+	       case 1: 
+	    	  emphour = 4;
+	          break;
+	      case 2:
+	    	  emphour = 8;
+	    	 break;
+	      default :
+		      emphour = 0;
+	       }
+		empWage = emphour * empRatePerHr;
+		System.out.println("Emp Wage "+ empWage);
+		empDay = empWage * empDay;
+		System.out.println("Emp Day Per Month  " + empDay);
 		
-		 int N = (int)(Math.random()*10) % 3;
-       switch(N)
-       {
-       case 1: 
-    	  part_time_hour = 4;
-    	  Cal =  part_time_hour * wage_per_hour;
-    	System.out.println(Cal);
-     	   break;
-      case 2:
-    	  full_day = 8;
-    	  Cal =  wage_per_hour * full_day;
-    	System.out.println(Cal);
-    	 break;
-      default : System.out.println("zero salary");
-     
     }
-  }
+ 
 }	
