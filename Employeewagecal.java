ebd8e3f8a489af490d04cc6be656385399f2ac65
unknown <akshayHVshinde04@gmail.com>
2021-09-26 10:12:51 +0530

    Added:CalculateParttimeEmployeeWageUC3

--- Employeewagecal.java
+++ Employeewagecal.java
@@ -3,21 +3,10 @@ package pratice;
 public class Employeewagecal {
 	public static void main(String[] args) { 
 		
-		
-		int wage_per_hour = 20;
-     	int full_day = 8;
-	    int Cal = wage_per_hour * full_day;
+	   int part_time_hour = 4;             
+      int wage_per_hour = 20;
+	  int Cal = part_time_hour * wage_per_hour  ;
 	    System.out.println(Cal);
-	
-		
-       int part_time_hour = 4;             
-       wage_per_hour = 20;
-	   Cal = part_time_hour * wage_per_hour  ;
-	    System.out.println(Cal);
-		
-		
-		
-		
-		
+				
  }
 }
