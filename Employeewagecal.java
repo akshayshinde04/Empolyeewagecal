f7186692472cb948c1ed9109e74669cb705bcce6
unknown <akshayHVshinde04@gmail.com>
2021-09-26 22:24:43 +0530

    Added: TotalworkhourUC6

--- Employeewagecal.java
+++ Employeewagecal.java
@@ -2,29 +2,31 @@ package pratice;
 
 public class Employeewagecal {
 	public static void main(String[] args) { 
-	
-		int empWage;
-		int empRatePerHr = 20;
-		int emphour = 0;
-		int wage_per_hour = 20;
-		int empDay = 20;
-	
-		 int empCheck = (int)(Math.random()*10) % 3;
-	       switch(empCheck)
-	       {
-	       case 1: 
-	    	  emphour = 4;
-	          break;
-	      case 2:
-	    	  emphour = 8;
-	    	 break;
-	      default :
-		      emphour = 0;
-	       }
-		empWage = emphour * empRatePerHr;
-		System.out.println("Emp Wage "+ empWage);
-		empDay = empWage * empDay;
-		System.out.println("Emp Day Per Month  " + empDay);
+		int totalSalary;
+		  int empRatePerHr = 20;
+		  int no_of_WorkingDays = 20;
+		  int totalWorkingDays = 0;
+		  int max_Hr_In_Month = 100;
+		  int empHr,totalEmpHrs = 0;
+		  while(totalEmpHrs < max_Hr_In_Month && totalWorkingDays < no_of_WorkingDays)
+		  {
+			  totalWorkingDays++;
+			  double empCheck = Math.floor(Math.random() * 10) % 3;
+			  int i = (int)empCheck;
+			  switch(i) {
+			  case 1:
+				  empHr = 8;
+				  break;
+			  case 2:
+				  empHr = 4;
+				  break;
+			 default:
+				 empHr = 0;
+			                 }
+	   totalEmpHrs = totalEmpHrs + empHr;
+		  }
+			 totalSalary = empRatePerHr * totalEmpHrs;
+			 System.out.println(" total Salary is:" + totalSalary);
 		
     }
  
