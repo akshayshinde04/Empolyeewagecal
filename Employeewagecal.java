0c8e5dbe78b708f6da3a3f932a13bf50645243d8
unknown <akshayHVshinde04@gmail.com>
2021-09-26 10:24:46 +0530

    Added:EmpolyeewagebySwitchcaseUC4

--- Employeewagecal.java
+++ Employeewagecal.java
@@ -2,11 +2,23 @@ package pratice;
 
 public class Employeewagecal {
 	public static void main(String[] args) { 
+		int part_time_hour,wage_per_hour =20,Cal,full_day;
 		
-	   int part_time_hour = 4;             
-      int wage_per_hour = 20;
-	  int Cal = part_time_hour * wage_per_hour  ;
-	    System.out.println(Cal);
-				
- }
-}
+		 int N = (int)(Math.random()*10) % 3;
+       switch(N)
+       {
+       case 1: 
+    	  part_time_hour = 4;
+    	  Cal =  part_time_hour * wage_per_hour;
+    	System.out.println(Cal);
+     	   break;
+      case 2:
+    	  full_day = 8;
+    	  Cal =  wage_per_hour * full_day;
+    	System.out.println(Cal);
+    	 break;
+      default : System.out.println("zero salary");
+     
+    }
+  }
+}	
