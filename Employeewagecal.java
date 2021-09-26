6b631f39d0903f77677cbb55779984e2f282cdf7
captjack-4 <90202533+captjack-4@users.noreply.github.com>
2021-09-26 10:06:28 +0530

    Update Employeewagecal.java

--- Employeewagecal.java
+++ Employeewagecal.java
@@ -8,16 +8,5 @@ public class Employeewagecal {
      	int full_day = 8;
 	    int Cal = wage_per_hour * full_day;
 	    System.out.println(Cal);
-	
-		
-       int part_time_hour = 4;             
-       wage_per_hour = 20;
-	   Cal = part_time_hour * wage_per_hour  ;
-	    System.out.println(Cal);
-		
-		
-		
-		
-		
  }
 }
