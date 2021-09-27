88e5e1903662a52ed240fa1faa4bae3b10630f58
captjack-4 <90202533+captjack-4@users.noreply.github.com>
2021-09-27 17:17:58 +0530

    Update Employeewagecal.java

--- Employeewagecal.java
+++ Employeewagecal.java
@@ -1,8 +1,8 @@
 package pratice;
 
 public class Employeewagecal {
-	public static void main(String[] args) { 
-		int totalSalary;
+	   public static void main(String[] args) { 
+		  int totalSalary;
 		  int empRatePerHr = 20;
 		  int no_of_WorkingDays = 20;
 		  int totalWorkingDays = 0;
@@ -10,23 +10,23 @@ public class Employeewagecal {
 		  int empHr,totalEmpHrs = 0;
 		  while(totalEmpHrs < max_Hr_In_Month && totalWorkingDays < no_of_WorkingDays)
 		  {
-			  totalWorkingDays++;
-			  double empCheck = Math.floor(Math.random() * 10) % 3;
-			  int i = (int)empCheck;
-			  switch(i) {
-			  case 1:
-				  empHr = 8;
-				  break;
-			  case 2:
-				  empHr = 4;
-				  break;
-			 default:
-				 empHr = 0;
+		     totalWorkingDays++;
+		     double empCheck = Math.floor(Math.random() * 10) % 3;
+	             int i = (int)empCheck;
+		      switch(i) {
+		       case 1:
+			      empHr = 8;
+		       break;
+		       case 2:
+			      empHr = 4;
+			break;
+		       default:
+			       empHr = 0;
 			                 }
-	   totalEmpHrs = totalEmpHrs + empHr;
+	      totalEmpHrs = totalEmpHrs + empHr;
 		  }
-			 totalSalary = empRatePerHr * totalEmpHrs;
-			 System.out.println(" total Salary is:" + totalSalary);
+	       totalSalary = empRatePerHr * totalEmpHrs;
+	       System.out.println(" total Salary is:" + totalSalary);
 		
     }
  
